c1f3387603cceb1bc53490ad41eab5929e2d23ef
Fernando <fernando@rastrocompleto.com.br>
2021-01-06 18:31:32 -0300

    validações

--- Consulta_Webservice_Correios/Program.cs
+++ Consulta_Webservice_Correios/Program.cs
@@ -15,46 +15,52 @@ namespace Consulta_Webservice_Correios
 
         public static void CalcularPrecoPrazo()
         {
-            //System.Console.Write("Digite o código do tipo de envio: ");
-            //var tipoEnvio = System.Console.ReadLine();
-            //System.Console.Write("Digite o CEP de origem: ");
-            //var cepOrigem = System.Console.ReadLine();
-            //System.Console.Write("Digite o CEP de destino: ");
-            //var cepDestino = System.Console.ReadLine();
-            //System.Console.Write("Digite o peso (kg): ");
-            //var peso = System.Console.ReadLine();
-            //System.Console.Write("Digite o código do formato (caixa, envelope, etc): ");
-            //var codigoFormato = System.Console.ReadLine();
-            //System.Console.Write("Digite o comprimento: ");
-            //var comprimento = System.Console.ReadLine();
-            //System.Console.Write("Digite a altura: ");
-            //var altura = System.Console.ReadLine();
-            //System.Console.Write("Digite a largura: ");
-            //var largura = System.Console.ReadLine();
-            //System.Console.Write("Digite o diâmetro: ");
-            //var diametro = System.Console.ReadLine();
-            //System.Console.Write("Entrega em mão própria (S/N)?: ");
-            //var maoPropria = System.Console.ReadLine();
-            //System.Console.Write("Digite o valor declarado: ");
-            //var valorDeclarado = System.Console.ReadLine();
-            //System.Console.Write("Aviso de recebimento (S/N)?: ");
-            //var avisoRecebimento = System.Console.ReadLine();
+            System.Console.Write("Digite o código do tipo de envio: ");
+            var nCdServico = System.Console.ReadLine();
+            System.Console.Write("Digite o CEP de origem: ");
+            var sCepOrigem = System.Console.ReadLine();
+            System.Console.Write("Digite o CEP de destino: ");
+            var sCepDestino = System.Console.ReadLine();
+            System.Console.Write("Digite o peso (kg): ");
+
+            var nVlPeso = System.Console.ReadLine();
+            while (nVlPeso.Contains(","))
+            {
+                System.Console.WriteLine("Utilize ponto(.) para separador de casas decimais");
+                System.Console.Write("Digite o peso (kg): ");
+                nVlPeso = System.Console.ReadLine();
+            }
+
+            System.Console.Write("Digite o código do formato (caixa, envelope, etc): ");
+            var nCdFormato = System.Console.ReadLine();
+
+            while (nCdFormato.Contains(",") || nCdFormato.Contains("."))
+            {
+                System.Console.WriteLine("O Código informado é inválido, digite 1 para Caixa / Pacote, 2 para Rolo / Prisma ou 3 para envelop");
+                System.Console.Write("Digite o código do formato (caixa, envelope, etc): ");
+                nCdFormato = System.Console.ReadLine();                
+            }          
+                
+        
+
+            System.Console.Write("Digite o comprimento: ");
+            var nVlComprimento = System.Console.ReadLine();
+            System.Console.Write("Digite a altura: ");
+            var nVlAltura = System.Console.ReadLine();
+            System.Console.Write("Digite a largura: ");
+            var nVlLargura = System.Console.ReadLine();
+            System.Console.Write("Digite o diâmetro: ");
+            var nVlDiametro = System.Console.ReadLine();
+            System.Console.Write("Entrega em mão própria (S/N)?: ");
+            var sCdMaoPropria = System.Console.ReadLine();
+            System.Console.Write("Digite o valor declarado: ");
+            var nVlValorDeclarado = System.Console.ReadLine();
+            System.Console.Write("Aviso de recebimento (S/N)?: ");
+            var sCdAvisoRecebimento = System.Console.ReadLine();
 
 
             string nCdEmpresa = string.Empty;
-            string sDsSenha = string.Empty;
-            string nCdServico = "04510";
-            string sCepOrigem = "39860000";
-            string sCepDestino = "29101786";
-            string nVlPeso = "0.020";
-            int nCdFormato = 3;
-            decimal nVlComprimento = 15;
-            decimal nVlAltura = 0;
-            decimal nVlLargura = 10;
-            decimal nVlDiametro = 0;
-            string sCdMaoPropria = "N";
-            decimal nVlValorDeclarado = 0;
-            string sCdAvisoRecebimento = "N";
+            string sDsSenha = string.Empty;            
 
             try
             {
@@ -62,12 +68,13 @@ namespace Consulta_Webservice_Correios
                 WSCorreiosCalculaPreco.CalcPrecoPrazoWSSoapClient ws = new WSCorreiosCalculaPreco.CalcPrecoPrazoWSSoapClient();
 
                 var resposta = ws.CalcPrecoPrazo(nCdEmpresa,sDsSenha,nCdServico,
-                    sCepOrigem,sCepDestino,nVlPeso, nCdFormato,nVlComprimento,
-                    nVlAltura,nVlLargura,nVlDiametro,sCdMaoPropria,nVlValorDeclarado,sCdAvisoRecebimento);
+                    sCepOrigem,sCepDestino,nVlPeso, int.Parse(nCdFormato),decimal.Parse(nVlComprimento),
+                    decimal.Parse(nVlAltura),decimal.Parse(nVlLargura),decimal.Parse(nVlDiametro),sCdMaoPropria.ToUpper(),decimal.Parse(nVlValorDeclarado),sCdAvisoRecebimento.ToUpper());
                 var respostaReal = resposta.Servicos.FirstOrDefault();
                 if (respostaReal != null)
                 {
-                    System.Console.WriteLine();
+                    System.Console.WriteLine("\n");
+                    System.Console.WriteLine("Entrega em mãos: {0}", (respostaReal.EntregaDomiciliar.Equals("N") ? "Não" : "Sim"));
                     System.Console.WriteLine("Prazo: {0} dia(s)", respostaReal.PrazoEntrega);
                     System.Console.WriteLine("Valor: R$ {0}", respostaReal.Valor);
                 }
